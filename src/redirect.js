6258a184a879ef263ab866b86f5030b577f3b72e
trondhumbor <trondhumbor@gmail.com>
2014-08-09 18:44:00 +0200

    Initial commit.

---
+++ src/redirect.js
@@ -0,0 +1,10 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+        function(m,key,value) {
+            vars[key] = value;
+        });
+    return vars;
+}
+
+window.location = getUrlVars()["url"];
